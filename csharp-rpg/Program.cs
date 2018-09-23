80d4d05311ed44d3eeb6daeb19a8373577b138d5
Michael Smith <msmithboise@gmail.com>
2018-09-23 14:53:50 -0600

    layout

--- csharp-rpg/Program.cs
+++ csharp-rpg/Program.cs
@@ -5,7 +5,6 @@ namespace csharp_rpg
     class Program
     {
         static void Main(string[] args)
-
         {
             Console.WriteLine("You reach a lit forest and see a deer drinking from a pond");
         }
