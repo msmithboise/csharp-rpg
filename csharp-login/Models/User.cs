8256c80277fb4b03ca1bc3be99bff2af293c4140
Michael Smith <msmithboise@gmail.com>
2018-09-25 10:26:18 -0600

    push

--- csharp-login/Models/User.cs
+++ csharp-login/Models/User.cs
@@ -1,5 +1,33 @@
+using System.Collections.Generic;
+
 namespace csharp_login.Models
 {
 
+    public class User
+    {
 
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public bool ValidatePassword(string password)
+        {
+            return password == Password;
+        }
+
+
+
+        public User(string name, string pass)  // constructor
+        {
+            Username = name;
+            Password = pass;
+        }
+
+
+
+
+
+
+
+
+    }
 }
--- csharp-login/Program.cs
+++ csharp-login/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using csharp_login.Models;
 
 namespace csharp_login
 {
     class Program
     {
+        private const string Value = "Welcome!";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Dictionary<string, User> users = new Dictionary<string, User>(){
+                { "mark", new User("mark", "IHeartCode")},
+                { "jake", new User("jake", "MarkRocks")}
+            };
+
+            // how to register new user
+            User dmoney = new User("d$", "DeMoneyMoMoney");
+            users.Add(dmoney.Username, dmoney);
+
+
+
+
+
+
+
+            Console.Clear();
+            Console.WriteLine("Please login to continue");
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+            if (users.ContainsKey(username) && users[username].ValidatePassword(password))
+            {
+                // users at username.ValidatePassword method we created, passing password property
+
+                System.Console.WriteLine("SUCCESS!!!!");
+
+            }
+            else
+            {
+                System.Console.WriteLine("INVALID USERNAME OR PASSWORD!");
+            }
+
+
+
         }
     }
 }
